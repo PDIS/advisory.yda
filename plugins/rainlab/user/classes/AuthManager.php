7f90957830c7b1eaadab7a8b9f12ee3674ea4105
s89206x <s89206x@gmail.com>
2018-08-12 23:50:20 +0800

    updata composer

--- plugins/rainlab/user/classes/AuthManager.php
+++ plugins/rainlab/user/classes/AuthManager.php
@@ -50,15 +50,14 @@ class AuthManager extends RainAuthManager
     /**
      * {@inheritDoc}
      */
-    public function register(array $credentials, $activate = false)
+    public function register(array $credentials, $activate = false, $autoLogin = true)
     {
         if ($guest = $this->findGuestUserByCredentials($credentials)) {
             return $this->convertGuestToUser($guest, $credentials, $activate);
         }
 
-        return parent::register($credentials, $activate);
+        return parent::register($credentials, $activate, $autoLogin);
     }
-
     //
     // Guest users
     //
