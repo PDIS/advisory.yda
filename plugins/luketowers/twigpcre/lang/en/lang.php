40da5064132c614e9277721bbebda096c4665d5b
mark19891107 <mark19891107@gmail.com>
2018-09-11 17:06:32 +0800

    add twig regex plugin

---
+++ plugins/luketowers/twigpcre/lang/en/lang.php
@@ -0,0 +1,6 @@
+<?php return [
+    'plugin' => [
+        'name'        => 'Twig PCRE (Regex)',
+        'description' => 'Extends Twig to support Pearl-Compatible-Regular-Expressions (Regex) through Twig filters and functions',
+    ],
+];
